9622aa920d2c7c4ab5b5ed37d6ef7a964df32698
Likhitha sree <likhithasree1109@gmail.com>
2025-02-16 09:48:53 +0530

    Add files via upload

---
+++ C#/Console Applications/28.cs
@@ -0,0 +1,11 @@
+﻿using System;
+int x;
+System.Console.WriteLine("Enter a value:");
+x = Convert.ToInt32(Console.ReadLine());
+if (x > 0)
+    if (x == 0)
+        System.Console.WriteLine("Zero");
+    else
+        System.Console.WriteLine("Positive");
+else
+    System.Console.WriteLine("Negitive");
