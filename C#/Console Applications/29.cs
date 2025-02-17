b238ced21ec5970f7a456fc6029fad8fb626aed3
Likhitha sree <likhithasree1109@gmail.com>
2025-02-17 09:36:11 +0530

    Add files via upload

---
+++ C#/Console Applications/29.cs
@@ -0,0 +1,8 @@
+﻿using System;
+int a, b;
+System.Console.WriteLine("Enter a number:");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter b number:");
+b=Convert.ToInt32(Console.ReadLine());
+int t=(a>b)?a:b;
+System.Console.WriteLine(t + "is big");
