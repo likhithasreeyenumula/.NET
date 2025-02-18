acaa938027d9402a716b3c0842854b665560813f
Likhitha sree <likhithasree1109@gmail.com>
2025-02-18 21:49:07 +0530

    Add files via upload

---
+++ C#/Console Applications/30.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Linq;
+int a, b, c, d, e;
+System.Console.WriteLine("Enter a:");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter b:");
+b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter c:");
+c = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter d:");
+d = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter e:");
+e = Convert.ToInt32(Console.ReadLine());
+int x = a;
+if (b > x)
+{
+    x= b; 
+}
+else if (c > x)
+{
+    x= c;
+}
+else if (d > x)
+{
+    x= d;
+}
+else if (e > x)
+{
+    x= e;
+}
+System.Console.WriteLine(x + "is big");
