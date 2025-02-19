a5af625a3521e8d82e5682f232cf46b137d59637
Likhitha sree <likhithasree1109@gmail.com>
2025-02-19 18:38:21 +0530

    Add files via upload

---
+++ C#/Console Applications/31.cs
@@ -0,0 +1,20 @@
+﻿using System;
+int a, b, c;
+System.Console.WriteLine("Enter A:");
+a =Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter B:");
+b =Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter C:");
+c =Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    System.Console.WriteLine("A is Big");
+}
+else if (b > c)
+{
+    System.Console.WriteLine("B is Big");
+}
+else
+{
+    System.Console.WriteLine("c is Big");
+}
