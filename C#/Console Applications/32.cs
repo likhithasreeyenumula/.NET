e4e64e1805c6a83f125c30d7cf2da4e41ccff01a
Likhitha sree <likhithasree1109@gmail.com>
2025-02-20 18:53:34 +0530

    Add files via upload

---
+++ C#/Console Applications/32.cs
@@ -0,0 +1,12 @@
+﻿using System;
+int x;
+System.Console.WriteLine("Enter A:");
+x=Convert.ToInt32(Console.ReadLine());
+if (x % 2 == 0)
+{
+    System.Console.WriteLine("even");
+}
+else
+{
+    System.Console.WriteLine("odd");
+}
