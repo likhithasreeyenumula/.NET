ce275dcc9250dbbe3c459b26d1ddbc8262820886
Likhitha sree <likhithasree1109@gmail.com>
2025-02-21 20:23:56 +0530

    Add files via upload

---
+++ C#/Console Applications/33.cs
@@ -0,0 +1,19 @@
+﻿using System;
+int age;
+System.Console.WriteLine("enter age:");
+age =Convert.ToInt32(Console.ReadLine());
+if (age >= 65)
+{
+    System.Console.WriteLine("senior citizen");
+    System.Console.WriteLine("Eligible for vote");
+}
+else if (age >= 65 && age >= 18)
+{
+    System.Console.WriteLine("Major citizen");
+    System.Console.WriteLine("Eligible for vote");
+}
+else
+{
+    System.Console.WriteLine("Minor citizen");
+    System.Console.WriteLine("Not Eligible for vote");
+}
