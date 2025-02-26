7536ee1316f01a6891aff14f6ddcda969bc0b646
Likhitha sree <likhithasree1109@gmail.com>
2025-02-26 19:07:19 +0530

    Add files via upload

---
+++ C#/Console Applications/38.cs
@@ -0,0 +1,15 @@
+﻿using System;
+int i = 1;
+while (i <= 10)
+{
+    System.Console.WriteLine(i);
+    if (i % 2 == 0)
+    {
+        System.Console.WriteLine("likki");
+    }
+    else
+    {
+        System.Console.WriteLine("126");
+    }
+    i = i + 1;
+}
