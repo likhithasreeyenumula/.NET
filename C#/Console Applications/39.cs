59d23f367357170fe43cf4d8b4047234b2c408d6
Likhitha sree <likhithasree1109@gmail.com>
2025-02-27 10:16:42 +0530

    Add files via upload

---
+++ C#/Console Applications/39.cs
@@ -0,0 +1,22 @@
+﻿using System;
+int i = 1;
+while (i <= 20)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.WriteLine("FuzzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        System.Console.WriteLine("Fuzz");
+    }
+    else if (i % 5 == 0)
+    {
+        System.Console.WriteLine("Buzz");
+    }
+    else
+    {
+        System.Console.WriteLine(i);
+    }
+    i = i + 1;
+}
