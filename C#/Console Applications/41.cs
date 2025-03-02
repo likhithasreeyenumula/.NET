7a7d132cce7a8db34e6bc8aae7946bc92e208a69
Likhitha sree <likhithasree1109@gmail.com>
2025-03-02 20:33:58 +0530

    Add files via upload

---
+++ C#/Console Applications/41.cs
@@ -0,0 +1,9 @@
+﻿using System;
+int n, i = 1;
+System.Console.WriteLine("Enter a number:");
+n=Convert.ToInt32(Console.ReadLine());
+while (i <= n)
+{
+    System.Console.WriteLine("\t"+i);
+    i = i + 1;
+}
