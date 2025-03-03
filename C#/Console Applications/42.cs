a0428fdaab20bd8fc605f87bbbda41a2917d8c63
Likhitha sree <likhithasree1109@gmail.com>
2025-03-03 10:21:15 +0530

    Add files via upload

---
+++ C#/Console Applications/42.cs
@@ -0,0 +1,10 @@
+﻿using System;
+int n, f = 1, i = 1;
+System.Console.WriteLine("Enter a number:");
+n=Convert.ToInt32(Console.ReadLine());
+while (i <= n)
+{
+    f = f * i;
+    i = i + 1;
+}
+System.Console.Write("Factriol of"+n+"is="+f);
