dabecef7bfb20df6b1c4c4a1ec321c16c74eb4fd
Likhitha sree <likhithasree1109@gmail.com>
2025-03-04 10:17:48 +0530

    Add files via upload

---
+++ C#/Console Applications/43.cs
@@ -0,0 +1,11 @@
+﻿using System;
+int n, f = 1, i = 1;
+System.Console.WriteLine("Enter a number:");
+n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Number Factorial");
+while (i <= n)
+{
+    f = f * i;
+    System.Console.WriteLine(i+"\t"+f);
+    i = i + 1;
+}
