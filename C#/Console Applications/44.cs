c45a78eaecb20fc50597a87578bf2f775c62db52
Likhitha sree <likhithasree1109@gmail.com>
2025-03-05 10:10:44 +0530

    Add files via upload

---
+++ C#/Console Applications/44.cs
@@ -0,0 +1,10 @@
+﻿using System;
+int n, c = 0, i = 1;
+System.Console.WriteLine("Enter a number:");
+n = Convert.ToInt32(Console.ReadLine());
+while (i <= n)
+{
+    n = n / 10;
+    c = c + 1;
+}
+System.Console.WriteLine("count="+c);
