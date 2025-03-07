571c36957ef54a6cb86f6278b08704f068bab4f5
Likhitha sree <likhithasree1109@gmail.com>
2025-03-07 11:52:43 +0530

    Add files via upload

---
+++ C#/Console Applications/46.cs
@@ -0,0 +1,11 @@
+﻿using System;
+int n, s = 0, r, i = 1;
+System.Console.WriteLine("Enter a number");
+n=Convert.ToInt32(Console.ReadLine());
+while (i <= n)
+{
+    r = n % 10;
+    s = s * 10 + r;
+    n= n / 10;
+}
+System.Console.WriteLine("Reverse" + s);
