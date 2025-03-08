6806b91722dd87be88e534ebef261d3c949f37e7
Likhitha sree <likhithasree1109@gmail.com>
2025-03-08 12:43:58 +0530

    Add files via upload

---
+++ C#/Console Applications/47.cs
@@ -0,0 +1,19 @@
+﻿using System;
+int n, t, r, s = 0, i = 1;
+System.Console.WriteLine("Enter a number:");
+n=Convert.ToInt32(Console.ReadLine());
+t = n;
+while (i <= n)
+{
+    r = n % 10;
+    s = s * 10 + r;
+    n = n / 10;
+}
+if (t == 5)
+{
+    System.Console.WriteLine("palindrome");
+}
+else
+{
+    System.Console.WriteLine("Not palindrome");
+}
