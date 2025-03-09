cd5adcc22ba2a51d3d0d23cd09a9779b7c16c38a
Likhitha sree <likhithasree1109@gmail.com>
2025-03-09 12:49:43 +0530

    Add files via upload

---
+++ C#/Console Applications/48.cs
@@ -0,0 +1,18 @@
+﻿using System;
+int n, l = 0, f = 0, i = 1;
+System.Console.WriteLine("Enter a number:");
+n=Convert.ToInt32(Console.ReadLine());
+l = n % 10;
+while (i <= n)
+{
+    f = n % 10;
+    n = n / 10;
+}
+if (f == 1)
+{
+    System.Console.WriteLine("cyclo");
+}
+else
+{
+    System.Console.WriteLine("Not Cyclo");
+}
