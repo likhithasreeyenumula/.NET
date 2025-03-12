4d2f8d61cc3efae725349e019594410c7c892ad6
Likhitha sree <likhithasree1109@gmail.com>
2025-03-12 13:14:51 +0530

    Add files via upload

---
+++ C#/Console Applications/51.cs
@@ -0,0 +1,15 @@
+﻿using System;
+int a, b, c;
+char ch;
+do
+{
+    System.Console.WriteLine("Enter a:");
+    a=Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Enter b:");
+    b=Convert.ToInt32(Console.ReadLine());
+    c = a + b;
+    System.Console.WriteLine("Sum:" + c);
+    System.Console.WriteLine("Do you want to do again");
+    ch=Convert.ToChar(Console.ReadLine());
+}
+while (ch=='Y'||ch=='y');
