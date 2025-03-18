5724308e339cb38eae4c1661a32a6420bd376f11
Likhitha sree <likhithasree1109@gmail.com>
2025-03-18 12:39:03 +0530

    Add files via upload

---
+++ C#/Console Applications/57.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+int[] a = new int[100];
+int n, i;
+System.Console.Write("Enter size of array");
+n=Convert.ToInt32(Console.ReadLine());
+for (i = 0; i < n; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.Clear();
+System.Console.WriteLine("Elements are");
+System.Console.WriteLine("-------------------");
+for (i = 0; i < n; i++)
+{
+    System.Console.Write("\t"+a[i]);
+}
