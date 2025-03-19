0b15b845d4f54f756c071b72d239dabe31e07705
Likhitha sree <likhithasree1109@gmail.com>
2025-03-19 21:09:38 +0530

    Add files via upload

---
+++ C#/Console Applications/58.cs
@@ -0,0 +1,23 @@
+﻿int[] a = new int[100];
+int n, i, c = 0, s = 0;
+double av;
+System.Console.WriteLine("enter size of array:");
+n = Convert.ToInt32(Console.ReadLine());
+for (i = 0; i < n; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+    c = c + 1;
+    s=s + a[i];
+}
+System.Console.Clear();
+System.Console.WriteLine("Count:" + c);
+System.Console.WriteLine("Sum:" + s);
+for (i = 0; i < n; i++)
+{
+    System.Console.WriteLine(i);
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Count:" + c);
+System.Console.WriteLine("Sum:" + s);
+av = s / c;
+System.Console.WriteLine("Average:" + av);
