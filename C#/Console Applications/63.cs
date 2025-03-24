3357af69ba63da2bd11e153eaff966c5c550d936
Likhitha sree <likhithasree1109@gmail.com>
2025-03-24 14:42:33 +0530

    Add files via upload

---
+++ C#/Console Applications/63.cs
@@ -0,0 +1,35 @@
+﻿using System;
+int[,] A = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+int[,] B= { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+int i, j;
+System.Console.WriteLine();
+System.Console.WriteLine("Matrix");
+System.Console.WriteLine("------------");
+for (i = 0; i < 3; i++)
+{
+    for(j = 0; j < 3; j++)
+        System.Console.WriteLine("\t"+A[i, j]);
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+System.Console.WriteLine("B Matrix");
+System.Console.WriteLine("-----------------");
+for(i = 0;i < 3; i++)
+{
+    for (j = 0;j < 3; j++)
+    {
+        System.Console.WriteLine("\t"+A[i, j]);
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+System.Console.WriteLine("A+B");
+System.Console.WriteLine("-----------------");
+for(i = 0;i<3; i++)
+{
+    for(j=0;j<3; j++)
+    {
+        System.Console.WriteLine("\t" + (A[i, j] + B[i, j]));
+        System.Console.WriteLine();
+    }
+}
