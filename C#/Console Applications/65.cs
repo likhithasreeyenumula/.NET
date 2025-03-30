a4984b5dd096b815f6e6963ac9bda65151613cfc
Likhitha sree <likhithasree1109@gmail.com>
2025-03-30 22:04:53 +0530

    Add files via upload

---
+++ C#/Console Applications/65.cs
@@ -0,0 +1,18 @@
+﻿using System;
+int [,] a= new int[100, 100];
+int r, c, i, j;
+System.Console.WriteLine("Enter rows:");
+r = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter col:");
+c = Convert.ToInt32(Console.ReadLine());
+for (i = 0; i < r; i++)
+{
+    for (j = 0; j < c; j++)
+        a[i, j] = Convert.ToInt32(Console.ReadLine());
+}
+for (i = 0; i < r; i++)
+{
+    for (j = 0; j < c; j++)
+        System.Console.WriteLine("\t" + a[i, j]);
+    System.Console.WriteLine();
+}
