39f7cc9fbc57973ca694f343e3bedbc0f512fca7
Likhitha sree <likhithasree1109@gmail.com>
2025-04-01 13:56:32 +0530

    Add files via upload

---
+++ C#/Console Applications/67.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public void putempdet()
+{
+    System.Console.WriteLine("Eno:" + e.eno);
+    System.Console.WriteLine("Ename:" + e.ename);
+    System.Console.WriteLine("Esal:" + e.esal);
+    System.Console.WriteLine("Egrade:" + e.egrade);   
+}
+class Test
+{
+    public static void Main()
+    {
+        Emp e = new Emp();
+        e.eno = 101;
+        e.ename = "Anil";
+        e.esal = 4321.75;
+        e.egrade = 'A';
+        e.putempdet();
+    }
+}
