29fdfaab8134e2624368fe4fa2531faf3bd46e8e
Likhitha sree <likhithasree1109@gmail.com>
2025-04-02 10:05:49 +0530

    Add files via upload

---
+++ C#/Console Applications/68.cs
@@ -0,0 +1,35 @@
+﻿using System;
+struct emp
+{
+    public int eno;
+    public string ename;
+    public double esalary;
+    public char egrade;
+    public void getempdet()
+    {
+        System.Console.WriteLine("Enter Eno:");
+        eno = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Enter Ename:");
+        ename = Console.ReadLine();
+        System.Console.WriteLine("Enter Esalary:");
+        esalary = Convert.ToDouble(Console.ReadLine());
+        System.Console.WriteLine("Enter Egrade:");
+        egrade = Convert.ToChar(Console.Read());
+    }
+    public void putempdet()
+    {
+        System.Console.WriteLine("Eno:" + eno);
+        System.Console.WriteLine("Ename:" + ename);
+        System.Console.WriteLine("Esalary:" + esalary);
+        System.Console.WriteLine("Egrade:" + egrade);
+    }
+}
+class Test
+{
+    public static void Main()
+    {
+        emp e=new emp();
+        e.getempdet();
+        e.putempdet();
+    }
+}
