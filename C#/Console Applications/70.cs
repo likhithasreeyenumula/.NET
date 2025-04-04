40455c1b4b35c5ecfce25db9f53c85b4d889f3e4
Likhitha sree <likhithasree1109@gmail.com>
2025-04-04 13:47:52 +0530

    Add files via upload

---
+++ C#/Console Applications/70.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Bird
+{
+    public void fly()
+    {
+        System.Console.WriteLine("fly");
+    }
+    public
+        {
+        Bird Parrot = new Bird();
+    Parrot.fly();
+        System.Console.WriteLine("-----------------------");
+        Bird Pigeon = new Bird();
+    Pigeon.fly();
+}
+}
+//we can instantiate any no.of objects
