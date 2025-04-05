fc8edb1261087611a6f35be718c30c5f342095de
Likhitha sree <likhithasree1109@gmail.com>
2025-04-05 17:20:41 +0530

    Add files via upload

---
+++ C#/Console Applications/71.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Bird
+{
+    public void fly()
+    {
+        System.Console.WriteLine("Fly");
+    }
+    public void buildNest()
+    {
+        System.Console.WriteLine("Nest");
+    }
+    public static void main(String[] args)
+    {
+        Bird parrot= new Bird();
+        parrot.fly();
+        parrot.buildNest();
+    }
+}
