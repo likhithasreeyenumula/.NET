96124bdca2823f943479aa04eb1d63eba1c05609
Likhitha sree <likhithasree1109@gmail.com>
2025-04-08 09:32:35 +0530

    Add files via upload

---
+++ C#/Console Applications/73.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Bird
+{
+    public int age;
+    public int wings;
+    public double wt;
+    public string color;
+    public void fly()
+    {
+        System.Console.WriteLine("fly");
+    }
+    public void Nest()
+    {
+        System.Console.WriteLine("Nest");
+    }
+    public static void main (string[] args)
+    {
+        Bird parrot = new Bird();
+        parrot.age = 5;
+        parrot.wings = 2;
+        parrot.wt = 5.2;
+        parrot.color = "Green";
+        System.Console.WriteLine("Age:"+parrot.age);
+        System.Console.WriteLine("Wings:"+parrot.wings);
+        System.Console.WriteLine("weight:" + parrot.wt);
+        System.Console.WriteLine("Color:"+parrot.color);
+        parrot.fly();
+        parrot.Nest();
+    }
+}
