2eba5bbe7b0b5766b42c20c805bb1683061504e4
Likhitha sree <likhithasree1109@gmail.com>
2025-04-10 10:25:13 +0530

    Add files via upload

---
+++ C#/Console Applications/75.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class mycalci
+{
+    int a, b;
+    public void mysum(int x, int y)
+    { 
+        a = x; 
+        b = y; 
+        System.Console.WriteLine("sum:"+(a+b));
+    }
+    public static void main()
+    {
+        mycalci mc = new mycalci();
+        mc.mysum(20, 10);
+    }
+}
