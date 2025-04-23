d9315ac9f79b18be44b13386fa0376a21ac9729f
Likhitha sree <likhithasree1109@gmail.com>
2025-04-23 15:06:29 +0530

    Add files via upload

---
+++ C#/Console Applications/78.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class IceCream
+{
+    public int price;
+    public int quantity;
+    public double weight;
+    public string color;
+
+    public void Melt()
+    {
+        Console.WriteLine("It melts");
+    }
+
+    public int BillAmount()
+    {
+        return price * quantity;
+    }
+
+    public static void Main()
+    {
+        IceCream vanilla = new IceCream();
+        vanilla.price = 20;
+        vanilla.quantity = 5;
+        vanilla.weight = 45.5;
+        vanilla.color = "White";
+
+        Console.WriteLine("Price: " + vanilla.price);
+        Console.WriteLine("Quantity: " + vanilla.quantity);
+        Console.WriteLine("Weight: " + vanilla.weight);
+        Console.WriteLine("Color: " + vanilla.color);
+
+        vanilla.Melt();
+        Console.WriteLine("Bill Amount: " + vanilla.BillAmount());
+    }
+}
