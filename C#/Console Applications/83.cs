e8f06ebda0bf49230c1fa08eea854736179c68f1
Likhitha sree <likhithasree1109@gmail.com>
2025-05-08 09:39:03 +0530

    Add files via upload

---
+++ C#/Console Applications/83.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Car
+{
+    string brand;
+    public Car(string theBrand)
+    {
+        brand = theBrand;
+    }
+    public Car(Car otherCar)
+    {
+        brand = otherCar.brand;
+    }
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("Bugatti");
+        Console.WriteLine("Brand of car: " + car1.brand);
+
+        Car carz = new Car(car1);
+        Console.WriteLine("Brand of car: " + carz.brand);
+    }
+}
