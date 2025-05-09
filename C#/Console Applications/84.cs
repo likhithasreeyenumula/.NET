34c23040b6596ba081a19665e4fd4277c6529fd4
Likhitha sree <likhithasree1109@gmail.com>
2025-05-09 16:04:49 +0530

    Add files via upload

---
+++ C#/Console Applications/84.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Rect
+{
+    public int l;
+    public int b;
+    public int area()
+    {
+        return l * b;
+    }
+    public static void main()
+    {
+        Rect R = new Rect();
+        R.l = 20;
+        R.b = 40;
+        System.Console.WriteLine("Area:"+R.area());
+    }
+}
