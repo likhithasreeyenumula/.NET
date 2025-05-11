319e3ac70a0b0ac4eb96192de829bde6391d2c6a
Likhitha sree <likhithasree1109@gmail.com>
2025-05-11 13:54:53 +0530

    Add files via upload

---
+++ C#/Console Applications/85.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class A
+{
+    public void fofca()
+    {
+        System.Console.WriteLine("function of class A");
+    }
+}
+class B : A
+{
+    public void fofcc()
+    {
+        System.Console.WriteLine("function of class c");
+    }
+    public static void main()
+    {
+        B objB=new B();
+        objB.fofca();
+        objB.fofcc();
+        System.Console.WriteLine("---------------------");
+        c objc = new c();
+        objc.fofca();
+        objc.fofcb();
+        objc.fofcc();
+    }
+}
