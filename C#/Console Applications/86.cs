47ebfa14ce692eae1096887d6608fe896639c4b1
Likhitha sree <likhithasree1109@gmail.com>
2025-05-12 17:29:20 +0530

    Add files via upload

---
+++ C#/Console Applications/86.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class A
+{
+    public void fofca()
+    {
+        System.Console.WriteLine("A");
+    }
+}
+class B:A
+{
+    public void fofcb()
+    {
+        System.Console.WriteLine("B");
+    }
+}
+class C : A
+{
+    public void fofcc()
+    {
+        System.Console.WriteLine("C");
+    }
+}
+class D : A
+{
+    public void fofcd()
+    {
+        System.Console.WriteLine("D");
+    }
+}
+public static void main();
