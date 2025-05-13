6a8e538808e97d6ef93518c527e9d97e5608e508
Likhitha sree <likhithasree1109@gmail.com>
2025-05-13 09:38:54 +0530

    Add files via upload

---
+++ C#/Console Applications/87.cs
@@ -0,0 +1,26 @@
+﻿using System;
+interface A
+{
+    void gretone();
+}
+interface B
+{
+    void grettwo();
+}
+class C : A, B
+{
+    public void greetone()
+    {
+        Console.WriteLine("Hello form greet one");
+    }
+    public void greettwo()
+    {
+        Console.WriteLine("hello from greet two");
+    }
+    public static void main()
+    {
+        C objc=new C();
+        objc.greetone();
+        objc.greettwo();
+    }
+}
