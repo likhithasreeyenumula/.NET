5b52c52664be4b19f10e4badf7dbec3bb48c7f93
Likhitha sree <likhithasree1109@gmail.com>
2025-05-14 10:30:11 +0530

    Add files via upload

---
+++ C#/Console Applications/88.cs
@@ -0,0 +1,27 @@
+﻿using System;
+abstract class Bank
+{
+    public abstract void itrate();
+}
+class SBI : Bank
+{
+    public override void itrate()
+    {
+        Console.WriteLine("SBI Bank Intrest rate is");
+    }
+}
+class HDFC : Bank
+{
+    public override void itrate()
+    {
+        Console.WriteLine("HDFC Bank Insterest rate is 9%");
+    }
+    public static void main()
+    {
+        Bank b;
+        b = new SBI();
+        b.itrate();
+        b=new HDFC();
+        b.itrate();
+    }
+}
